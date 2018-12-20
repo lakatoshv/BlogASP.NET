1d5252dd9c65fff918da9b465eb734bc9e2f321e
lakatoshvv <some.pochta1@gmail.com>
2018-12-20 21:59:50 +0200

    Created PostViewModel into ViewModels

---
+++ Blog/ViewModels/Posts/PostViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.ViewModels.Posts
+{
+    public class PostViewModel
+    {
+        public Post post { get; set; }
+        public IList<Comment> comments { get; set; }
+    }
+}
