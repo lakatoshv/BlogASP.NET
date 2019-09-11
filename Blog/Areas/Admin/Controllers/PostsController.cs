f1585bf04fb7eb34cb6c04d8b8d3fd78d7331bc4
Vitaliy Lakatosh <vlakatosh@netfully.com>
2019-09-11 18:19:18 +0300

    Create Post from admin

--- Blog/Areas/Admin/Controllers/PostsController.cs
+++ Blog/Areas/Admin/Controllers/PostsController.cs
@@ -1,6 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
 using Blog.Areas.Admin.Services.Posts;
 using Blog.Models;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using WebGrease.Css.Extensions;
 
 namespace Blog.Areas.Admin.Controllers
 {
@@ -17,18 +23,60 @@ namespace Blog.Areas.Admin.Controllers
             return View(posts);
         }
 
-        // GET: Admin/Posts/Create
+        // GET: Posts/Create
         [HttpGet]
+        [Authorize]
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Admin/Posts/Create
+        // POST: Posts/Create
         [HttpPost]
         public ActionResult Create(Post postModel)
         {
-            return View();
+            try
+            {
+                //post.CreatedAt = DateTime.Now;
+                if (ModelState.IsValid)
+                {
+                    postModel.CreatedAt = DateTime.Now;
+                    postModel.Author = User.Identity.GetUserId();
+                    _db.Posts.Add(postModel);
+                    _db.SaveChanges();
+
+                    if (!postModel.Tags.IsNullOrWhiteSpace())
+                    {
+                        String[] tags = postModel.Tags.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        foreach (var tag in tags)
+                        {
+                            var tagForAdd = new Tag()
+                            {
+                                Title = tag,
+                                PostId = postModel.Id
+                            };
+                            _db.Tags.Add(tagForAdd);
+
+                        }
+                    }
+                    _db.SaveChanges();
+                    _db.Tags.Where(tag => tag.PostId.Equals(postModel.Id)).ForEach(
+                        t =>
+                        {
+                            postModel.PostTags.Add(t);
+                        }
+
+                    );
+                    _db.Entry(postModel).State = EntityState.Modified;
+                    _db.SaveChanges();
+                    return RedirectToAction("index", "Posts", new { area = "Admin" });
+                }
+                return null;
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         // GET: Admin/Posts/Edit/5
