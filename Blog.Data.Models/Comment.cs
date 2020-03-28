b57182441b39fc06022cff88fd70b063ffac1c2e
Віталій Лакатош <some.pochta1@gmail.com>
2020-03-28 09:20:03 +0200

    Create Data.Models project with docblock

---
+++ Blog.Data.Models/Comment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Blog.Data.Models
+{
+    /// <summary>
+    /// Comment model.
+    /// </summary>
+    public class Comment
+    {
+        /// <summary>
+        /// Gets or sets id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets postID.
+        /// </summary>
+        public int PostID { get; set; }
+
+        /// <summary>
+        /// Gets or sets author.
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// Gets or sets commentBody.
+        /// </summary>
+        public string CommentBody { get; set; }
+
+        /// <summary>
+        /// Gets or sets createdAt.
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets likes.
+        /// </summary>
+        public int Likes { get; set; }
+
+        /// <summary>
+        /// Gets or sets dislikes.
+        /// </summary>
+        public int Dislikes { get; set; }
+    }
+}
---
+++ Blog.Data.Models/Message.cs
@@ -0,0 +1,38 @@
+﻿namespace Blog.Data.Models
+{
+    /// <summary>
+    /// Message model.
+    /// </summary>
+    public class Message
+    {
+        /// <summary>
+        /// Gets or sets id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets applicationUser.
+        /// </summary>
+        public string ApplicationUser { get; set; }
+
+        /// <summary>
+        /// Gets or sets email.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets subject.
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// Gets or sets body.
+        /// </summary>
+        public string Body { get; set; }
+    }
+}
