471f6530c3093594854ba5576686af59f6c69d1d
Віталій Лакатош <some.pochta1@gmail.com>
2020-05-08 20:52:20 +0300

    Create Blog.Data.Core with dockblock

---
+++ Blog.Data.Core/Models/Interfaces/IAuditInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Blog.Data.Core.Models.Interfaces
+{
+    /// <summary>
+    /// Audit info interface.
+    /// </summary>
+    public interface IAuditInfo
+    {
+        /// <summary>
+        /// Gets or sets created on.
+        /// </summary>
+        DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// Gets or sets modified on.
+        /// </summary>
+        DateTime? ModifiedOn { get; set; }
+    }
+}
---
+++ Blog.Data.Core/Models/Interfaces/IDeletableEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Blog.Data.Core.Models.Interfaces
+{
+    /// <summary>
+    /// Deletable entity interface.
+    /// </summary>
+    public interface IDeletableEntity
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether is deleted.
+        /// </summary>
+        bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets deleted on.
+        /// </summary>
+        DateTime? DeletedOn { get; set; }
+    }
+}
