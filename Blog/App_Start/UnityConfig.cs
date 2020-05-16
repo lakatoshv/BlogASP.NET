cacbacc50271748a92f341ad2eebf9a2081dceaf
Віталій Лакатош <some.pochta1@gmail.com>
2020-05-16 22:51:49 +0300

    Add Unity.Mvc5

---
+++ Blog/App_Start/UnityConfig.cs
@@ -0,0 +1,37 @@
+using System.Web.Mvc;
+using Blog.Data.Models;
+using BLog.Data.Repository;
+using BLog.Data.Repository.Interfaces;
+using Blog.Services;
+using Blog.Services.Interfaces;
+using Blog.Services.Posts;
+using Blog.Services.Posts.Interfaces;
+using Unity;
+using Unity.Mvc5;
+
+namespace Blog
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<ICommentsService, CommentsService>();
+            container.RegisterType<IMessagesService, MessagesService>();
+            container.RegisterType<IPostsService, PostsService>();
+            container.RegisterType<IProfilesService, ProfilesService>();
+            container.RegisterType<ITagsService, TagsService>();
+
+
+            container.RegisterType<IRepository<Comment>, Repository<Comment>>();
+            container.RegisterType<IRepository<Message>, Repository<Message>>();
+            container.RegisterType<IRepository<Post>, Repository<Post>>();
+            container.RegisterType<IRepository<Profile>, Repository<Profile>>();
+            container.RegisterType<IRepository<Tag>, Repository<Tag>>();
+            container.RegisterType<IRepository<ApplicationUser>, Repository<ApplicationUser>>();
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
