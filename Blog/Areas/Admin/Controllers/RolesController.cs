1f0a40171bd84d6fa6e721c9857dd19fd04410c0
Віталій Лакатош <some.pochta1@gmail.com>
2021-07-14 15:29:24 +0300

    Display roles list

---
+++ Blog/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,55 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Blog.Areas.Admin.ViewModels;
+using BLog.Data;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Blog.Areas.Admin.Controllers
+{
+    /// <summary>
+    /// Roles controller.
+    /// </summary>
+    /// <seealso cref="Controller" />
+    [Authorize(Roles = "Administrator")]
+    public class RolesController : Controller
+    {
+        /// <summary>
+        /// The role manager.
+        /// </summary>
+        private RoleManager<IdentityRole> _roleManager;
+
+        /// <summary>
+        /// Gets the role manager.
+        /// </summary>
+        /// <value>
+        /// The role manager.
+        /// </value>
+        public RoleManager<IdentityRole> RoleManager
+        {
+            get
+            {
+                return _roleManager ?? new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new BlogContext()));
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets all roles.
+        /// </summary>
+        /// <returns>ActionResult</returns>
+        // GET: Admin/Roles
+        public async Task<ActionResult> Index()
+        {
+            ViewBag.Controller = "Posts";
+            ViewBag.UploadFileViewModel = new UploadFileViewModel();
+
+            var roles = await RoleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+    }
+}
---
+++ Blog/public/js/pages/roles/index.js
@@ -0,0 +1,26 @@
+﻿function setDeleteRoleValues(id) {
+    $("#delete-id").val(id);
+}
+
+function deleteRole(url) {
+    var id = $("#delete-id").val();
+    ajaxQuery(id, url);
+}
+
+$("select").change(function() {
+    $(this).next("button").show();
+});
+
+$('.upload-btn').on('click', function() {
+    $('.upload-form').toggle();
+});
+
+function cheked(select) {
+    // берём значение из select и что-то с ним делаем
+    return select.val();
+};
+
+$("#search-btn").click(function() {
+    var search = $("#search").val();
+    location.href = '@Url.Action("Index", "Posts")?search=' + search;
+});
